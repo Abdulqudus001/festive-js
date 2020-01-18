e43969c0a980600e68bad2f004eddaeeb4ab30df
Abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-01-18 20:04:32 +0100

    Added santas socks to nav-logo

---
+++ festive.js
@@ -0,0 +1,6 @@
+document.addEventListener('load', () => {
+  const transformPage = (() => {
+    // Selects navbar with class of "festive-nav"(Assuming there is only one navbar 😉)
+    const navbar = document.querySelector('.festive-nav');
+  })();
+});
